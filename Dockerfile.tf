a6657b83873985c658ec98787c2323bf221bd3f6
dpressel <dpressel@gmail.com>
2020-04-27 09:22:47 -0400

    add TF1 dockerfile

---
+++ Dockerfile.tf
@@ -0,0 +1,31 @@
+FROM meadml/cuda10.0-cudnn7-devel-ubuntu16.04-python3.6
+
+COPY xpctl /usr/mead/xpctl
+COPY mead-baseline /usr/mead/mead-baseline
+
+WORKDIR /usr/mead
+ADD XPCTL_VERSION .
+ADD VERSION .
+
+RUN cd xpctl/ && pip install -e .
+RUN cd mead-baseline/layers && pip install -e .
+RUN cd mead-baseline && pip install -e .[test,yaml]
+
+# Set env variables
+# Set baseline logging vars
+ENV TIMING_LOG_LEVEL=DEBUG
+# Set terminal encodings
+ENV LC_ALL=C.UTF-8
+ENV LANG=C.UTF-8
+# Set ENV to tensorflow can find cuda
+ENV PATH=/usr/local/cuda/bin:$PATH
+ENV LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
+# Install baseline
+COPY . /usr/mead
+
+# Install tensorflow
+RUN python3.6 -m pip install tensorflow-gpu==1.14.0 && \
+    python3.6 -m pip install tensorflow-hub==0.4.0
+
+ENTRYPOINT ["mead-train", "--config", "config/sst2.json"]
